09d32b0c933ff66262d63fb96415313ba950f1fd
Lorenzo Sciandra <notkelset@kelset.dev>
2025-03-23 16:15:02 +0000

    cleanup and reorder things accordingly

--- src/commands/piracy-acts.ts
+++ src/commands/piracy-acts.ts
@@ -1,35 +1,79 @@
 export const PiracyActs = [
+  // Purple
   {
     name: 'kaido-drop',
     bounty: 5000,
     description: 'Play a 10-drop Purple Kaido and kill at least 7 characters',
     category: 'Purple'
   },
-  {
-    name: 'emperor-slayer',
-    bounty: 3000,
-    description:
-      'Kill any of: 10 drop Big Mom, 10 drop Kaido, 9 drop Shanks, 9 drop Edward Newgate',
-    category: 'Generic'
-  },
-  {
-    name: 'treasure-hunter',
-    bounty: 1000,
-    description: 'Play 4 searches (Leader abilities, card that puts more cards in your hand)',
-    category: 'Generic'
-  },
+
+  // Black
   {
     name: 'admirals-orders',
     bounty: 3000,
     description: 'Playing a Black deck, K.O. 5 characters through effect (e.g. Lucci\'s On-Play)',
     category: 'Black'
   },
+
+  // Red
   {
     name: 'two-year-training',
     bounty: 5000,
     description: 'Jet Pistol a Pacifista',
     category: 'Red'
   },
+  {
+    name: 'hayai',
+    bounty: 2000,
+    description: 'Play 2 characters with Rush on the same turn',
+    category: 'Red'
+  },
+
+  // Blue
+  {
+    name: 'cat-burglar',
+    bounty: 2000,
+    description:
+      'Win a game as OP03-040 Nami leader using secondary win con (No cards in deck)',
+    category: 'Blue'
+  },
+  {
+    name: 'red-roc',
+    bounty: 3000,
+    description: 'Bottom 3 characters with a Blue Event',
+    category: 'Blue'
+  },
+
+  // Yellow
+  {
+    name: 'o-nami',
+    bounty: 5000,
+    description: 'Successfully banish using O-Nami on-play ability',
+    category: 'Yellow'
+  },
+  {
+    name: 'satellite',
+    bounty: 5000,
+    description:
+      'Have 4 different Vegapunk Satellite characters on the board whilst playing Vegapunk Leader',
+    category: 'Yellow'
+  },
+
+  // Green
+  {
+    name: 'best-leader',
+    bounty: 5000,
+    description: 'Win a game with Hody Jones Leader OP06-20',
+    category: 'Green'
+  },
+  {
+    name: 'king-of-hell',
+    bounty: 5000,
+    description: 'Successfully land all 3 swings with OP06-118 Zoro',
+    category: 'Green'
+  },
+
+  // Multi-colour
   {
     name: 'straw-hat',
     bounty: 5000,
@@ -45,49 +89,10 @@ export const PiracyActs = [
     category: 'Multi-colour'
   },
   {
-    name: 'true-nakama',
-    bounty: 3000,
-    description: 'Block for your teammate in Buddy Battle',
-    category: 'Generic'
-  },
-  {
-    name: 'honesty-impact',
-    bounty: 15000,
-    description: 'K.O. a stage',
-    category: 'Generic'
-  },
-  {
-    name: 'cat-burglar',
-    bounty: 2000,
-    description:
-      'Win a game as OP03-040 Nami leader using secondary win con (No cards in deck)',
-    category: 'Blue'
-  },
-  {
     name: 'shogun',
     bounty: 5000,
     description: 'Have 4 different characters from the country of Wano on the board',
     category: 'Multi-colour'
-  },
-  {
-    name: 'one-mans-trash',
-    bounty: 5000,
-    description:
-      'Excluding Nami OP02 leader, have 40+ cards in your graveyard at any point during a game',
-    category: 'Generic'
-  },
-  {
-    name: 'assemble',
-    bounty: 3000,
-    description:
-      'Flood the board with 4 characters in a chain play (e.g. ST-13 Dadan and Film package)',
-    category: 'Generic'
-  },
-  {
-    name: 'sadge',
-    bounty: 5000,
-    description: 'One of your Ace characters gets K.O.d',
-    category: 'Generic'
   },
   {
     name: 'dressrosa',
@@ -115,41 +120,51 @@ export const PiracyActs = [
     description: 'Play the 3 brothers (adult version) on the same turn (Luffy, Ace, Sabo)',
     category: 'Multi-colour'
   },
+
+  // Generic
   {
-    name: 'hayai',
-    bounty: 2000,
-    description: 'Play 2 characters with Rush on the same turn',
-    category: 'Red'
-  },
-  {
-    name: 'red-roc',
+    name: 'emperor-slayer',
     bounty: 3000,
-    description: 'Bottom 3 characters with a Blue Event',
-    category: 'Blue'
+    description:
+      'Kill any of: 10 drop Big Mom, 10 drop Kaido, 9 drop Shanks, 9 drop Edward Newgate',
+    category: 'Generic'
   },
   {
-    name: 'o-nami',
-    bounty: 5000,
-    description: 'Successfully banish using O-Nami on-play ability',
-    category: 'Yellow'
+    name: 'treasure-hunter',
+    bounty: 1000,
+    description: 'Play 4 searches (Leader abilities, card that puts more cards in your hand)',
+    category: 'Generic'
   },
   {
-    name: 'satellite',
+    name: 'true-nakama',
+    bounty: 3000,
+    description: 'Block for your teammate in Buddy Battle',
+    category: 'Generic'
+  },
+  {
+    name: 'honesty-impact',
+    bounty: 15000,
+    description: 'K.O. a stage',
+    category: 'Generic'
+  },
+  {
+    name: 'one-mans-trash',
     bounty: 5000,
     description:
-      'Have 4 different Vegapunk Satellite characters on the board whilst playing Vegapunk Leader',
-    category: 'Yellow'
+      'Excluding Nami OP02 leader, have 40+ cards in your graveyard at any point during a game',
+    category: 'Generic'
   },
   {
-    name: 'best-leader',
-    bounty: 5000,
-    description: 'Win a game with Hody Jones Leader OP06-20',
-    category: 'Green'
+    name: 'assemble',
+    bounty: 3000,
+    description:
+      'Flood the board with 4 characters in a chain play (e.g. ST-13 Dadan and Film package)',
+    category: 'Generic'
   },
   {
-    name: 'king-of-hell',
+    name: 'sadge',
     bounty: 5000,
-    description: 'Successfully land all 3 swings with OP06-118 Zoro',
-    category: 'Green'
+    description: 'One of your Ace characters gets K.O.d',
+    category: 'Generic'
   }
 ];
