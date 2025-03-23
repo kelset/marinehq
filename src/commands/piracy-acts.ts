048ada08337079ad74a2c313635758d6614bd89b
Lorenzo Sciandra <notkelset@kelset.dev>
2025-03-23 16:07:45 +0000

    update list

--- src/commands/piracy-acts.ts
+++ src/commands/piracy-acts.ts
@@ -2,65 +2,154 @@ export const PiracyActs = [
   {
     name: 'kaido-drop',
     bounty: 5000,
-    description: 'Kill 7 Characters with Purple 10-drop Kaido'
+    description: 'Play a 10-drop Purple Kaido and kill at least 7 characters',
+    category: 'Purple'
   },
   {
     name: 'emperor-slayer',
     bounty: 3000,
     description:
-      'Kill any of: 10 drop Big Mom, 10 drop Kaido, 9 drop Shanks, 9 drop Edward Newgate'
+      'Kill any of: 10 drop Big Mom, 10 drop Kaido, 9 drop Shanks, 9 drop Edward Newgate',
+    category: 'Generic'
   },
   {
     name: 'treasure-hunter',
     bounty: 1000,
-    description: 'Play 4 searcher cards'
+    description: 'Play 4 searches (Leader abilities, card that puts more cards in your hand)',
+    category: 'Generic'
   },
   {
     name: 'admirals-orders',
     bounty: 3000,
-    description: 'Playing a Black deck, K.O. 5 characters through effects'
+    description: 'Playing a Black deck, K.O. 5 characters through effect (e.g. Lucci\'s On-Play)',
+    category: 'Black'
   },
   {
     name: 'two-year-training',
-    bounty: 3000,
-    description: 'Jet Pistol a Pacifista'
+    bounty: 5000,
+    description: 'Jet Pistol a Pacifista',
+    category: 'Red'
   },
   {
     name: 'straw-hat',
     bounty: 5000,
-    description: 'Have 5 characters on the board that are different Straw Hats'
+    description:
+      'Have 4 different strawhat on the board that is also a different canonical character from your leader who is a strawhat',
+    category: 'Multi-colour'
   },
   {
     name: 'conqueror-haki',
     bounty: 5000,
     description:
-      'Have different 5 characters on the board that canonically have used Conquerors Haki'
+      'Have five different characters/ leader on the board that canonically have conqueror\'s haki',
+    category: 'Multi-colour'
   },
   {
     name: 'true-nakama',
     bounty: 3000,
-    description: 'Block for your teammate in Buddy Battle'
-  },
-  {
-    name: 'local-king',
-    bounty: 3000,
-    description: 'Win a local tournament'
+    description: 'Block for your teammate in Buddy Battle',
+    category: 'Generic'
   },
   {
     name: 'honesty-impact',
     bounty: 15000,
-    description: 'K.O. a Stage'
+    description: 'K.O. a stage',
+    category: 'Generic'
   },
   {
     name: 'cat-burglar',
     bounty: 2000,
     description:
-      'Win a game with OP03-040 Nami Leader secondary win condition: deck reaching zero cards'
+      'Win a game as OP03-040 Nami leader using secondary win con (No cards in deck)',
+    category: 'Blue'
   },
   {
     name: 'shogun',
+    bounty: 5000,
+    description: 'Have 4 different characters from the country of Wano on the board',
+    category: 'Multi-colour'
+  },
+  {
+    name: 'one-mans-trash',
+    bounty: 5000,
+    description:
+      'Excluding Nami OP02 leader, have 40+ cards in your graveyard at any point during a game',
+    category: 'Generic'
+  },
+  {
+    name: 'assemble',
     bounty: 3000,
     description:
-      'Have 5 characters on the board that are different Wano people'
+      'Flood the board with 4 characters in a chain play (e.g. ST-13 Dadan and Film package)',
+    category: 'Generic'
+  },
+  {
+    name: 'sadge',
+    bounty: 5000,
+    description: 'One of your Ace characters gets K.O.d',
+    category: 'Generic'
+  },
+  {
+    name: 'dressrosa',
+    bounty: 5000,
+    description:
+      'Have 4 different Dressrosa character on the board that is also different canonical character from your leader who is a Dressrosa',
+    category: 'Multi-colour'
+  },
+  {
+    name: 'warlords',
+    bounty: 5000,
+    description:
+      'Have 4 different Warlords of the Seven Sea character on the board that is also different canonical character from your leader who is a Warlord',
+    category: 'Multi-colour'
+  },
+  {
+    name: 'vinsmoke',
+    bounty: 5000,
+    description: 'Transform 4 different Vinsmoke family members (Activation: Main)',
+    category: 'Multi-colour'
+  },
+  {
+    name: 'three-brothers',
+    bounty: 7000,
+    description: 'Play the 3 brothers (adult version) on the same turn (Luffy, Ace, Sabo)',
+    category: 'Multi-colour'
+  },
+  {
+    name: 'hayai',
+    bounty: 2000,
+    description: 'Play 2 characters with Rush on the same turn',
+    category: 'Red'
+  },
+  {
+    name: 'red-roc',
+    bounty: 3000,
+    description: 'Bottom 3 characters with a Blue Event',
+    category: 'Blue'
+  },
+  {
+    name: 'o-nami',
+    bounty: 5000,
+    description: 'Successfully banish using O-Nami on-play ability',
+    category: 'Yellow'
+  },
+  {
+    name: 'satellite',
+    bounty: 5000,
+    description:
+      'Have 4 different Vegapunk Satellite characters on the board whilst playing Vegapunk Leader',
+    category: 'Yellow'
+  },
+  {
+    name: 'best-leader',
+    bounty: 5000,
+    description: 'Win a game with Hody Jones Leader OP06-20',
+    category: 'Green'
+  },
+  {
+    name: 'king-of-hell',
+    bounty: 5000,
+    description: 'Successfully land all 3 swings with OP06-118 Zoro',
+    category: 'Green'
   }
 ];
